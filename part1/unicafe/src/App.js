7ec4c582d59280bbd929c92db7fbdfae888674f5
RDWE <50463277+RDWE@users.noreply.github.com>
2023-07-12 16:07:23 +0100

    Exercise 1.10

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,6 +6,14 @@ const Button = (props) => {
   )
 }
 
+const StatisticLine = (props) => { 
+  return ( 
+    <div>
+      {props.text} {props.value}
+    </div>
+  )
+}
+
 const Statistics = (props) => {
   if (props.good === 0 && props.neutral === 0 &&  props.bad === 0) 
     return (
@@ -18,12 +26,14 @@ const Statistics = (props) => {
     return ( 
     <div>
       <h1>Statistics</h1>
-      <p>Good: {props.good}</p>
-      <p>Neutral: {props.neutral}</p>
-      <p>Bad: {props.bad}</p>
-      <p>All: {props.good + props.neutral + props.bad}</p>
-      <p>Average: {(props.good - props.bad) / ( props.good + props.neutral + props.bad ) }</p>
-      <p>Positive: {props.good / (props.good + props.neutral + props.bad) * 100 } %</p>
+      <StatisticLine text="good" value={props.good}/>
+      <StatisticLine text="neutral" value={props.neutral}/>
+      <StatisticLine text="bad" value={props.bad} />
+      <StatisticLine text="all" value={props.good + props.neutral + props.bad} />
+      <StatisticLine text="average" value={(props.good - props.bad) / (props.good + props.neutral + props.bad)} />
+      <div>
+      <StatisticLine text="positive" value={((props.good) / (props.good + props.neutral + props.bad)) * 100 + ' %'} />
+      </div>
     </div>
     )
 } 
